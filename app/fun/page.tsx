c6d5356ac84fb6024f6325d39dcb149580e5ff90
l3x3d <admin@falsezero.io>
2023-04-26 17:26:31 -0700

    new file:   app/fun/page.tsx 	modified:   components/sidebar.tsx

---
+++ app/fun/page.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+const FunPage: FC = () => {
+  return (
+    <section>
+      <h1 className="font-bold text-3xl font-serif"> for fun</h1>
+
+    </section>
+  );
+};
+
+export default FunPage;
