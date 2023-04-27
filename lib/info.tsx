e86c51518390d2bd5aed539fec37f216ac804d9d
l3x3d <admin@falsezero.io>
2023-04-27 08:40:02 -0700

    modified:   lib/info.tsx

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,31 +1,18 @@
-import me from '../app/avatar.jpg';
+import me from '../app/placeholder.jpg';
 
-export const name = 'Jameson Taylor';
+export const name = 'Your Name';
 export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hi! I'm a Technology Consultant & Developer based in sunny southern california.
+      Hey, I'm a developer who loves Next.js. I work at BigCo or CoolStartup as a Software Engineer.
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-       
-
-  
-
-
-       Over the past 10 years, I've been at the forefront of the intersection between technology and fashion, witnessing and contributing to the remarkable evolution of e-commerce. As a specialist in the global high-fashion market, my passion lies in crafting headless experiences for top-tier clients.
-
-Considered an adaptive leader with a solid reputation for success in technology planning, deployment, management, and innovation. My proficiency extends across computer systems and e-commerce, grounded in a comprehensive understanding of contemporary software stacks. Embracing creativity and bold thinking, I'm always ready to make informed decisions and offer progressive solutions in the ever-changing tech landscape.
-
-My extensive background in web development and high-fashion web commerce allows me to achieve outstanding results by harnessing my distinct blend of technical and creative abilities. 
-
-As I continue to develop and broaden my expertise in the tech industry, I'm excited to share my insights and experiences through my personal page.
-
-Please feel free to explore my work and reach out to me for potential collaborations or projects. I'm always eager to connect and forge new partnerships.
+      When I'm not coding, I'm probably playing video games or watching anime. I also love to travel and explore new places.
     </>
   );
 };
